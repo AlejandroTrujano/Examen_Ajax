6e01502cae996d279fd97c5247ae3cc28225d0e9
AlejandroTrujano <ac3873832@gmail.com>
2024-06-16 23:00:11 -0600

    Add project files.

---
+++ Examen_Ajax/Controllers/Examen.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Examen_Ajax.Controllers
+{
+    public class Examen : Controller
+    {
+        public IActionResult Ajax ()
+        {
+            return View();
+        }
+    }
+}
